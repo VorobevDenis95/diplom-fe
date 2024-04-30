6ceae4ffc07d59c20a1afd9a821896741ef37b33
Denis <kurtcobaine192@outlook.com>
2024-05-01 00:09:40 +0300

    fix bugs

--- src/components/SelectionOfSeatsCarriage/RailwayCarriage/MapContainerRailwayCarriage/RailwaysCarriage/ThirthRailwayCarriage/ThirthRailwayCarriage.tsx
+++ src/components/SelectionOfSeatsCarriage/RailwayCarriage/MapContainerRailwayCarriage/RailwaysCarriage/ThirthRailwayCarriage/ThirthRailwayCarriage.tsx
@@ -1,24 +1,24 @@
-import { SeatsProps } from '../../../../../../shared/types/typesSeats';
-import SeatPositionThirth from '../../SeatsPosition/SeatPositionThirth/SeatPositionThirth';
-import './ThirthRailwayCarriage.css'
+// import { SeatsProps } from '../../../../../../shared/types/typesSeats';
+// import SeatPositionThirth from '../../SeatsPosition/SeatPositionThirth/SeatPositionThirth';
+// import './ThirthRailwayCarriage.css'
 
-export interface RailwayCarriageItemProps {
-  seats: SeatsProps[]
-}
+// export interface RailwayCarriageItemProps {
+//   seats: SeatsProps[]
+// }
 
-const ThirthRailwayCarriage = ({seats} :RailwayCarriageItemProps) => {
-  // const seats = []
+// const ThirthRailwayCarriage = ({seats} :RailwayCarriageItemProps) => {
+//   // const seats = []
   
-  console.log(seats)
+//   console.log(seats)
 
-  return (
-    <>
-    <div className='railway-carriage-map'>
-      <SeatPositionThirth seats={seats}/>
-    </div>
-</>
-  )
-}
+//   return (
+//     <>
+//     <div className='railway-carriage-map'>
+//       <SeatPositionThirth seats={seats}/>
+//     </div>
+// </>
+//   )
+// }
 
-export default ThirthRailwayCarriage;
+// export default ThirthRailwayCarriage;
 
