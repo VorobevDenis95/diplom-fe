c14c34101486bd5dc7551d8a0888aac257542ab4
Denis <124994219+VorobevDenis95@users.noreply.github.com>
2024-05-09 17:12:49 +0300

    Update CheckBoxContainer.tsx

--- src/components/SettingsContainer/CheckBoxContainerDetalils/CheckBoxContainer.tsx
+++ src/components/SettingsContainer/CheckBoxContainerDetalils/CheckBoxContainer.tsx
@@ -1,11 +1,11 @@
 import './CheckBoxContainer.css';
 import CheckBoxItem from './CheckboxItem/CheckboxItem';
-import coupeIcon from '../../../assets/images/svg/ railway__carriage/coupe.svg';
-import reservedSeat from '../../../assets/images/svg/ railway__carriage/reserved__seat.svg';
-import sedentary from '../../../assets/images/svg/ railway__carriage/sedentary.svg';
-import luxary from '../../../assets/images/svg/ railway__carriage/luxary.svg';
-import wifiIcom from '../../../assets/images/svg/ railway__carriage/wifi.svg';
-import expressIcon from '../../../assets/images/svg/ railway__carriage/express.svg';
+import coupeIcon from '../../../assets/images/svg/railway__carriage/coupe.svg';
+import reservedSeat from '../../../assets/images/svg/railway__carriage/reserved__seat.svg';
+import sedentary from '../../../assets/images/svg/railway__carriage/sedentary.svg';
+import luxary from '../../../assets/images/svg/railway__carriage/luxary.svg';
+import wifiIcom from '../../../assets/images/svg/railway__carriage/wifi.svg';
+import expressIcon from '../../../assets/images/svg/railway__carriage/express.svg';
 
 
 
