d8e6a5d0dc18b004cd2a45d5a492ca47ef8405a4
Denis <kurtcobaine192@outlook.com>
2024-05-20 23:22:31 +0300

    add icons background image in input search form

---
+++ src/components/modal/ModalContainer/ModalErrorBody/ModalErrorBody.tsx
@@ -0,0 +1,17 @@
+const ModalErrorBody = () => {
+  return (
+    <div className="modal__error-body">
+      <p className='modal__error-body__text-main'>
+        Таким образом консультация с широким активом
+        в значительной степени обуславливает создание модели развития.
+      </p>
+      <p className='modal__error-body__text'>
+        Повседневная практика показывает,
+        что сложившаяся структура организации играет важную роль в формировании
+        существенных финансовых и административных
+      </p>
+    </div>
+  )
+}
+
+export default ModalErrorBody;
---
+++ src/components/modal/ModalContainer/ModalInfoBody/ModalInfoBody.tsx
@@ -0,0 +1,21 @@
+import './ModalInfoBody.css';
+
+const ModalInfoBody = () => {
+  return (
+    <div className="modal__info-body">
+      <p className='modal__info-body__text'>
+        Таким образом консультация с широким активом в значительной степени
+        обуславливает создание модели развития.
+
+      </p>
+      <p className='modal__info-body__text'>
+        Повседневная практика показывает,
+        что сложившаяся структура организации играет важную роль в формировании
+        существенных финансовых и административных
+      </p>
+    </div>
+  )
+}
+
+export default ModalInfoBody;
+
